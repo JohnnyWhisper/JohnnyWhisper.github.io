7c2fef436bd7250edd1b71059ec3b55042d3a3a5
JohnnyWhisper <92794546+JohnnyWhisper@users.noreply.github.com>
2022-04-13 23:03:21 +0300

    Update task-08.js

--- goit-js-hw-06/js/task-08.js
+++ goit-js-hw-06/js/task-08.js
@@ -2,23 +2,6 @@ const loginFormEl = document.querySelector(".login-form");
 console.log(loginFormEl);
 
 
-
-const emailInputEl = loginFormEl.firstElementChild;
-console.log(emailInputEl);
-
-
-const emailPasswordEl = emailInputEl.nextElementSibling;
-console.log(emailPasswordEl);
-
-const BtnEl = loginFormEl.lastElementChild;
-console.log(BtnEl);
-
-
-console.log(loginFormEl.elements);
-
-
-
-
 loginFormEl.addEventListener('submit', (event) => {
     event.preventDefault();
 
@@ -37,5 +20,5 @@ loginFormEl.addEventListener('submit', (event) => {
     
     console.log(userData);
 
-   loginFormEl.reset();
+   event.currentTarget.reset();
 })
