87d2965e032966d39fa0ac51518b94b1a41f7e17
JohnnyWhisper <92794546+JohnnyWhisper@users.noreply.github.com>
2022-04-20 00:13:08 +0300

    Update task-01.js

--- goit-js-hw-06/js/task-01.js
+++ goit-js-hw-06/js/task-01.js
@@ -1,15 +1,14 @@
 const categoriesEl = document.querySelector("#categories");
 
 
-
 const categories = categoriesEl.querySelectorAll("li.item");
 console.log("Number of categories:", categories.length);
 
 for (let i=0; i < categories.length; i+=1) {
-   const category = categories[i].querySelector("h2");
 
-   const elements = category.nextElementSibling.querySelectorAll("li");
-  
+    const category = categories[i].firstElementChild;
+   const elements = category.nextElementSibling.children;
+
     console.log("Category:", category.textContent);
     console.log("Elements:", elements.length);
    
