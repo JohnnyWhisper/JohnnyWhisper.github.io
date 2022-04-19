752fe45f027ba92308772a94bede747f82383e72
JohnnyWhisper <92794546+JohnnyWhisper@users.noreply.github.com>
2022-04-20 01:36:09 +0300

    Update task-06.js

--- goit-js-hw-06/js/task-06.js
+++ goit-js-hw-06/js/task-06.js
@@ -1,13 +1,10 @@
 const inputEl = document.querySelector("#validation-input");
 
 
-
 const addRemoveClasslist = (remove, add) => {
   inputEl.classList.remove(`${remove}`);
   inputEl.classList.add(`${add}`);
 };
-
-
 
  const checkValidation = (event) => {
   if (Number(inputEl.dataset.length) === event.currentTarget.value.length) {
