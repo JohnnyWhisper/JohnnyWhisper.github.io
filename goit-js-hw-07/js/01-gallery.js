31dc35f0ec113b8d80da573b6f565020dca76012
JohnnyWhisper <92794546+JohnnyWhisper@users.noreply.github.com>
2022-04-23 03:54:40 +0300

    task1-completed

--- goit-js-hw-07/js/01-gallery.js
+++ goit-js-hw-07/js/01-gallery.js
@@ -1,4 +1,49 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+
+const createGalleryItems = ({preview, original, description}) => {
+    const galleryEl = document.createElement("div");
+    galleryEl.insertAdjacentHTML("afterbegin", `<a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>`);
+  
+
+  return galleryEl;
+}
+
+const galleryElements = galleryItems.map(createGalleryItems);
+galleryList.append(...galleryElements);
+
+
+galleryList.addEventListener ("click", selectPicture);
+
+
+function selectPicture (event) {
+    event.preventDefault();
+
+    if (event.target.classList.contains(".gallery")) {
+        return;
+    }
+
+    const bigPictureUrl = event.target.dataset.source;
+
+
+    const bigPicture = basicLightbox.create (`
+    <img src="${bigPictureUrl}" width="800" height="600">
+    `, {
+        onShow: (bigPicture) => {
+            window.onkeydown = bigPicture.close
+        }
+    });
+
+    bigPicture.show()
+
+}
+
