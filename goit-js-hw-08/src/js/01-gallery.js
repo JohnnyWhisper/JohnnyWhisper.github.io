436bb0963f1564b5aa9b584ca57f0b119683d634
JohnnyWhisper <92794546+JohnnyWhisper@users.noreply.github.com>
2022-05-06 21:33:50 +0300

    start

--- goit-js-hw-08/src/js/01-gallery.js
+++ goit-js-hw-08/src/js/01-gallery.js
@@ -3,3 +3,40 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+
+const createGalleryItems = ({preview, original, description}) => {
+    const galleryEl = document.createElement("li");
+    galleryEl.insertAdjacentHTML("afterbegin", `<a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>`);
+
+  return galleryEl;
+ 
+}
+
+const galleryElements = galleryItems.map(createGalleryItems);
+galleryList.append(...galleryElements);
+
+galleryList.addEventListener ("click", selectPicture);
+
+const captionOptions = {
+    captionSelector: 'img', 
+    captionType: 'attr',
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+    enableKeyboard: true,
+    uniqueImages: true,
+   };
+
+
+
+function selectPicture (event) {
+    event.preventDefault();   
+}
