60a0893e0fc07dfe406c52cdd35f198dbc596d55
JohnnyWhisper <92794546+JohnnyWhisper@users.noreply.github.com>
2022-05-07 03:04:36 +0300

    2nd-task

--- goit-js-hw-08/src/js/02-video.js
+++ goit-js-hw-08/src/js/02-video.js
@@ -1,1 +1,22 @@
+import vimeoPlayer from "@vimeo/player";
 
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+
+player.on('timeupdate', function(data) {
+
+const currentTime = data.seconds;
+console.log(currentTime);
+localStorage.setItem("videoplayer-current-time", `${currentTime}`)
+});
+
+
+player.setCurrentTime(0.01).then(function(seconds) {
+    seconds = localStorage.getItem("videoplayer-current-time");
+    console.log(seconds);
+    
+    player.setCurrentTime(seconds);
+
+})
