c694cb73cdba8a2708d1aec4ca433ca75ec3e7e3
JohnnyWhisper <92794546+JohnnyWhisper@users.noreply.github.com>
2022-05-08 02:56:50 +0300

    Update 02-video.js

--- goit-js-hw-08/src/js/02-video.js
+++ goit-js-hw-08/src/js/02-video.js
@@ -1,22 +1,14 @@
-import vimeoPlayer from "@vimeo/player";
 
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 
 
-player.on('timeupdate', function(data) {
-const currentTime = data.seconds;
-console.log(currentTime);
-localStorage.setItem("videoplayer-current-time", `${currentTime}`)
-});
+player.on('timeupdate', throttle(onPlay, 1000));
 
+function onPlay({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', seconds);
+}
 
-player.setCurrentTime(0.01).then(function(seconds) {
-    seconds = localStorage.getItem("videoplayer-current-time");
-    console.log(seconds);
-    
-    player.setCurrentTime(seconds);
-
-})
-
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
